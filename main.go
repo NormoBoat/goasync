be47cc85d687a3d0238a3702db9a356b935d3772
NormoBoat <normoboat@gmail.com>
2026-05-13 11:13:11 +0300

    pet-1-2

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	web "net/url"
 	"os"
+	"path"
 )
 
 func main() {
@@ -13,4 +19,37 @@ func main() {
 
 	output := os.Args[1]
 	urls := os.Args[2:]
+
+	for _, files := range urls {
+		downloadFile(files, output)
+	}
+}
+
+func downloadFile(url, savePath string) error {
+	if err := os.MkdirAll(savePath, os.ModeDir); err != nil {
+		return errors.New("Ошибка создания каталока загрузки")
+	}
+
+	resp, _ := http.Get(url)
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Сервер вернул: %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	u, err := web.Parse(url)
+	if err != nil {
+		return err
+	}
+
+	filename := path.Base(u.Path)
+
+	file, err := os.Create(savePath + "/" + filename)
+	if err != nil {
+		return fmt.Errorf("Не удалось создать файл для сохранения: %s\n", err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+
+	return nil
 }
